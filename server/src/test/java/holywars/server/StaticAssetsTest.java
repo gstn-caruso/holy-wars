b205f782c2091793cdd034cdd73d6f5f0565b792
Gastón Caruso <gstn.caruso@gmail.com>
2026-09-16 20:43:23 -0300

    feat(server): add the advisors frieze background placeholder svg
    Claude-Session: fb0ecccc-5e4f-45b4-acd9-a0660592df1c

--- server/src/test/java/holywars/server/StaticAssetsTest.java
+++ server/src/test/java/holywars/server/StaticAssetsTest.java
@@ -1,6 +1,8 @@
 package holywars.server;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -24,5 +26,14 @@ class StaticAssetsTest {
     @Test
     void servesTheLiveUpdatesScript() throws Exception {
         mockMvc.perform(get("/js/holy-wars.js")).andExpect(status().isOk());
+    }
+
+    @Test
+    void friezeFrameSvgIsServedWithItsFixedDimensions() throws Exception {
+        mockMvc.perform(get("/img/ui-frieze-advisors.svg"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("viewBox=\"0 0 394 130\"")))
+                .andExpect(content().string(containsString("width=\"394\"")))
+                .andExpect(content().string(containsString("height=\"130\"")));
     }
 }
